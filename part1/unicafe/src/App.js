7057e30e99df37f2dac64276f120afc3530954c7
Carlos Eduardo da Veiga Ferreira <kaduh5589@gmail.com>
2023-03-15 12:03:06 -0300

    feat: uniface exercise

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,24 +1,90 @@
 import { useState } from "react";
 
-const Display = ({ counter }) => <div>{counter}</div>;
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>{text}</button>
-);
+const StatisticsLine = ({ total, text }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{total}</td>
+    </tr>
+  );
+};
+
+const Statistics = ({ good, neutral, bad, all, average, positive }) => {
+  if (all === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h1>statistics</h1>
+      <table>
+        <thead>
+          <tr>
+            <th colSpan="2"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <StatisticsLine total={good} text="good"></StatisticsLine>
+          <StatisticsLine total={neutral} text="neutral"></StatisticsLine>
+          <StatisticsLine total={bad} text="bad"></StatisticsLine>
+          <StatisticsLine total={all} text="all"></StatisticsLine>
+          <StatisticsLine total={average} text="average"></StatisticsLine>
+          <StatisticsLine
+            total={positive + "%"}
+            text="positive"
+          ></StatisticsLine>
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter - 1);
-  const setToZero = () => setCounter(0);
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  let all = good + neutral + bad;
+  let average = (good * 1 + neutral * 0 + bad * -1) / all;
+  let positive = (good / all) * 100;
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button handleClick={increaseByOne} text="plus" />
-      <Button handleClick={setToZero} text="zero" />
-      <Button handleClick={decreaseByOne} text="minus" />
+      <h1>give feedback</h1>
+      <Button
+        handleClick={() => {
+          setGood(good + 1);
+        }}
+        text="good"
+      ></Button>
+      <Button
+        handleClick={() => {
+          setNeutral(neutral + 1);
+        }}
+        text="neutral"
+      ></Button>
+      <Button
+        handleClick={() => {
+          setBad(bad + 1);
+        }}
+        text="bad"
+      ></Button>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={all}
+        average={average}
+        positive={positive}
+      ></Statistics>
     </div>
   );
 };
