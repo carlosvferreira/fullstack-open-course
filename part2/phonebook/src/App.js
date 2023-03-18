f0328392ee52f484522aeb0acc90d0a4de88ebe2
Carlos Eduardo da Veiga Ferreira <kaduh5589@gmail.com>
2023-03-18 01:21:23 -0300

    feat: phonebook exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,14 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "040-1234567", id: 1 }]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterValue, setFilterValue] = useState("");
   const [personsToDisplay, setPersonsToDisplay] = useState(persons);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+      setPersonsToDisplay(response.data);
+    });
+  }, []);
 
   const addPerson = (e) => {
     e.preventDefault();
